5d3ee22f232a62d89b6e1ebe88a0e321bc18700f
Daniel Thornton <dshaggy28@yahoo.com>
2023-09-14 17:26:04 -0700

    started on player and level 01 scenes

---
+++ scripts/player.gd
@@ -0,0 +1,9 @@
+extends CharacterBody2D
+
+@export var speed = 150
+
+func _physics_process(delta):
+	var direction = Vector2(Input.get_axis("move_left", "move_right"),
+	Input.get_axis("move_up", "move_down"))
+	velocity = direction * speed
+	move_and_slide()
